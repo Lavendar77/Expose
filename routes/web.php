563fe3cd975e65731c8cf9102c5965ded0ed9627
Adeyinka M. Adefolurin <folurinyinka@gmail.com>
2019-11-14 11:57:01 +0100

    Test notification
    web.php - to preview in browser

--- routes/web.php
+++ routes/web.php
@@ -18,3 +18,8 @@ Route::get('/', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Route::get('mail', function () {
+	return (new App\Notifications\UserRegistered())
+                ->toMail(App\Models\User::find(1));
+});
