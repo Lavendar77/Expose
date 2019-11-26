67c587b3cb4d6964d9aa23369658422cafaf037e
Adeyinka M. Adefolurin <folurinyinka@gmail.com>
2019-11-26 18:01:48 +0100

    Passport prep

--- config/app.php
+++ config/app.php
@@ -166,6 +166,7 @@ return [
          * Package Service Providers...
          */
         Cedextech\TraitGenerator\ServiceProvider::class,
+        Barryvdh\Cors\ServiceProvider::class,
 
         /*
          * Application Service Providers...
